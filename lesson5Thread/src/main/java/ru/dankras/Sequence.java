7e2684ccdf991797ddb3856656cb251587135ce1
dmitry <krasnykhda@gmail.com>
2022-12-02 01:10:53 +0300

    Lesson3 commit1

---
+++ lesson5Thread/src/main/java/ru/dankras/Sequence.java
@@ -0,0 +1,84 @@
+package ru.dankras;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class Sequence {
+    private static final Logger logger = LoggerFactory.getLogger(Sequence.class);
+    private int nextThreadId = 0;
+    private final int maxNumberOfThreads;
+    private char[] symbols;
+    private Thread[] threads;
+    private int count = 1;
+
+    private void start() {
+        for (int id = 0; id < maxNumberOfThreads; id++) {
+            int curentId = id;
+            threads[curentId] = new Thread(() -> action(curentId, 2));
+            threads[curentId].start();
+        }
+
+    }
+
+    public synchronized void action(int currentId, int maxCount) {
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                while (nextThreadId != currentId) {
+                    this.wait();
+                }
+                if (count > maxCount) {
+                    logger.info("Before interrupt");
+                    Thread.currentThread().interrupt();
+                } else {
+                    logger.info(String.valueOf(symbols[currentId]));
+                }
+                setNextThreadId();
+                notifyAll();
+                logger.info("after notify");
+
+
+            } catch (InterruptedException ex) {
+                logger.info("thread interrupt");
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    private void setNextThreadId() {
+        if (nextThreadId >= 0) {
+            nextThreadId++;
+        }
+        if (nextThreadId == maxNumberOfThreads) {
+            nextThreadId = 0;
+            count++;
+        }
+    }
+
+
+    public Sequence(int maxNumberOfThreads) {
+        char startSymbol = 'a';
+        int ascii = (int) startSymbol;
+        symbols = new char[maxNumberOfThreads];
+        threads = new Thread[maxNumberOfThreads];
+        for (int i = 0; i < maxNumberOfThreads; i++) {
+            symbols[i] = (char) ascii;
+            ascii++;
+        }
+        this.maxNumberOfThreads = maxNumberOfThreads;
+    }
+
+    public static void main(String[] args) {
+        Sequence sequence = new Sequence(5);
+        sequence.start();
+    }
+
+    private static void sleep() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            logger.error(e.getMessage());
+            Thread.currentThread().interrupt();
+        }
+    }
+}

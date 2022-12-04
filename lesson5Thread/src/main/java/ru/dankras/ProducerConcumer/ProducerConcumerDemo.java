10ae61bcc0d6116eb22f2b6447c87041b7d29ef9
dmitry <krasnykhda@gmail.com>
2022-12-04 12:58:19 +0300

    Lesson3 commit1

--- lesson5Thread/src/main/java/ru/dankras/ProducerConcumer/ProducerConcumerDemo.java
+++ lesson5Thread/src/main/java/ru/dankras/ProducerConcumer/ProducerConcumerDemo.java
@@ -5,18 +5,8 @@ public class ProducerConcumerDemo {
         Box box=new Box();
         Producer producer=new Producer(box);
         Consumer consumer=new Consumer(box);
-        Thread thread1=new Thread(new Runnable() {
-            @Override
-            public void run() {
-                producer.produce();
-            }
-        });
-        Thread thread2=new Thread(new Runnable() {
-            @Override
-            public void run() {
-                consumer.consume();
-            }
-        });
+        Thread thread1=new Thread(() -> producer.produce());
+        Thread thread2=new Thread(() -> consumer.consume());
         thread1.start();
         thread2.start();
         thread1.join();
